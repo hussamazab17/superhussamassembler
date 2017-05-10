184cd3f217fa41847cb81fd5e4cd91a5d1bd048b
191829 <191829@S007RMC122-13.ads.friscoisd.org>
2017-05-10 12:20:01 -0500

    a whole bunch o' stuffs

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,8 @@
 
+import java.awt.Graphics;
+import java.awt.Rectangle;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Game {
 
@@ -9,6 +12,36 @@ public class Game {
 		frame = new JFrame("Super Hassam Assembler");
 		frame.setSize(1920, 1080);
 		frame.setUndecorated(true);
+		
+		final AnimationSquare ANI = new AnimationSquare(new Rectangle(0, 0, 40, 40));
+		
+		JPanel panel = new JPanel() {		
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.setColor(ANI.getColor());
+				g.drawRect((int) ANI.getX(), (int)ANI.getY(), 
+						(int) ANI.getHeight(), (int) ANI.getWidth());
+			}
+		};
+		
+		panel.addMouseListener(ANI);
+		panel.addMouseMotionListener(ANI);
+		
+		Thread t = new Thread() {
+			public void run() {
+				while(true) {
+					try {
+						Thread.sleep(1000 / 60);
+					} catch(Exception ex) {}
+					
+					frame.repaint();
+				}
+			}
+		};
+		
+		t.start();
+		
+		frame.add(panel);
 		
 		frame.setAlwaysOnTop(true);
 		frame.setVisible(true);
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,20 @@
-public class Player {
+
+import java.awt.KeyEventDispatcher;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+
+public class Player implements KeyEventDispatcher {
+	
+	private Rectangle r;
+	
+	public Player(Rectangle r) {
+		this.r = r;
+	}
+	
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent ke) {
+		System.out.println(ke.getKeyChar() + " " + ke.getKeyCode());
+		return false;
+	}
 
 }
